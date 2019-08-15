8d704118ddd093cacd8b21b389f3bc1f4609921b
quevivasbien <42875695+quevivasbien@users.noreply.github.com>
2019-08-14 19:24:03 -0600

    generalized functionality

--- get_sources.py
+++ get_sources.py
@@ -17,6 +17,14 @@ import json
 import authenticate
 
 
+CENSUS_PTTRN = r'[Cc]ensus'
+DEATH_PTTRN = r'[Dd]eath'
+CENSUS_COLUMNS = 'census_columns.json'
+DEATH_RECORD_COLUMNS = 'death_record_columns.json'
+
+ark_re = re.compile(r'[^:]{4}-[^:]{3}$')
+
+
 def log_warning(message, origin=None, load=None, log_file='log.txt'):
     """Print a warning message and save warning info to a log file.
 
@@ -103,13 +111,25 @@ def create_df(response, arkid):
     arkid (str): The ark ID of the requested resource
     """
     # Create dictionary based on JSON response
-    source_dict, c = iterate(response.json())
+    response_dict = response.json()
+    source_dict, c = iterate(response_dict)
     # Convert to Pandas DataFrame
     for k in source_dict.keys():
         source_dict[k] = source_dict[k].split(';')
     df = pd.DataFrame.from_dict(source_dict, orient='index').transpose()
     df['is_person'] = [int(i == c) for i in range(len(df))]
-    df['ark_id'] = [arkid]*len(df)
+    # TODO: Record the specific arkids for each entry in the record
+    try:
+        arkids = [p['identifiers']['http://gedcomx.org/Persistent'][0] for p in response_dict['persons']]
+        arkids = [ark_re.search(x).group() for x in arkids]
+        assert len(arkids) == len(df)
+    except (KeyError, AssertionError, AttributeError):
+        arkids = [None]*len(df)
+        try:
+            arkids[c] = arkid
+        except IndexError:
+            pass
+    df['ark_id'] = arkids
     return df
 
 
@@ -180,46 +200,73 @@ class FamilySearchSourcer:
         response = requests.get(url, headers=self.headers)
         return self.process_response(response, self.search_for_sources, pid, list, lambda x, _: x.json()['entries'])
 
-    def find_census(self, pid):
-        """Takes a PID and gets the ark IDs of the census records that person appears in."""
+    def check_attached_sources(self, pid, lookfor):
+        """Gets ark ids for records attached to a person that have a given word/pattern in their descriptions
+
+        pid (str): the PID of the person to get records from
+        lookfor (str): a regular expression to look for in record descriptions
+        """
         arkids = []
-        # Get Ark IDs for censuses already attached
         sources = self.get_attached_sources(pid)
         for source in sources:
-            if re.search(r'[Cc]ensus', ' '.join(x['value'] for x in source['titles'])):
+            if re.search(lookfor, ' '.join(x['value'] for x in source['titles'])):
                 try:
-                    arkids.append(re.search(r'[^:]{4}-[^:]{3}$', source['about']).group())
+                    arkids.append(ark_re.search(source['about']).group())
                 except (KeyError, AttributeError):
                     continue
-        # Search for more records
+        return arkids
+
+    def check_other_sources(self, pid, lookfor):
+        """Like check_attached_sources, but searches for as-yet-unattached records instead of looking at attached ones
+
+        pid (str): the PID of the person to get records from
+        lookfor (str): a regular expression to look for in record descriptions
+        """
+        arkids = []
         sources = self.search_for_sources(pid)
         for source in sources:
-            if re.search(r'[Cc]ensus', source['title']):
-                arkid = re.search(r'[^:]{4}-[^:]{3}$', source['id'])
+            if re.search(lookfor, source['title']):
+                arkid = ark_re.search(source['id'])
                 if arkid is not None:
                     arkids.append(arkid.group())
         return arkids
 
-    def process_census(self, arkid):
-        """Takes the ark ID for a census record and creates a Pandas DataFrame of the data on the record."""
+    def check_all_sources(self, pid, lookfor):
+        return self.check_attached_sources(pid, lookfor) + self.check_other_sources(pid, lookfor)
+
+    def process_record(self, arkid):
+        """Takes the ark ID for a record and creates a Pandas DataFrame of the data on the record."""
         url = f'https://api.familysearch.org/platform/records/personas/{arkid}'
         response = requests.get(url, headers=self.headers)
-        return self.process_response(response, self.process_census, arkid, pd.DataFrame, create_df)
+        return self.process_response(response, self.process_record, arkid, pd.DataFrame, create_df)
 
-    def get_census_for_pid(self, pid):
-        """Takes a PID and creates a Pandas DataFrame with all the data for the census records that person appears in"""
+    def get_records_for_pid(self, pid, lookfor):
+        """Gets record data for a person with the record descriptions matching a given word/pattern
+
+        pid (str): the person to look for records for
+        lookfor (str): a regular expression to look for in record descriptions (e.g. r'[Cc]ensus' for census records)
+        """
         print(f'Working on {pid}...')
-        arkids = self.find_census(pid)
+        arkids = self.check_all_sources(pid, lookfor)
         if arkids:
-            df = pd.concat((self.process_census(arkid) for arkid in arkids), sort=True)
+            df = pd.concat((self.process_record(arkid) for arkid in arkids), sort=True)
             df['PID'] = [pid]*len(df)
             return df
         else:
             return pd.DataFrame()
 
 
-def condense_census(df_in, columns_file='census_columns.json'):
-    """Takes a Pandas DataFrame of census data and merges/drops some of the columns to create a more compact dataset
+def process_year(yr):
+    if isinstance(yr, int):
+        return yr
+    elif isinstance(yr, str):
+        return int(yr[-4:])
+    else:
+        return yr
+
+
+def condense_record(df_in, columns_file):
+    """Takes a Pandas DataFrame of record data and merges/drops some of the columns to create a more compact dataset
     The column names to be retained and merged together are in the columns_file in JSON format.
     """
     with open(columns_file, 'r') as fh:
@@ -227,26 +274,66 @@ def condense_census(df_in, columns_file='census_columns.json'):
     df_out = pd.DataFrame(columns=list(columndict.keys()))
     for k in columndict.keys():
         for x in columndict[k]:
-            df_out[k] = df_out[k].combine_first(df_in[x])
+            try:
+                df_out[k] = df_out[k].combine_first(df_in[x])
+            except KeyError:
+                pass
+    return df_out.drop_duplicates()
+
+
+def condense_census(df_in):
+    df_out = condense_record(df_in, CENSUS_COLUMNS)
+    # Drop data from state censuses & where year is missing
+    df_out.year = df_out.year.apply(process_year)
+    df_out = df_out[pd.notna(df_out.year) & (df_out.year % 10 == 0)]
+    return df_out
+
+
+def condense_death_records(df_in):
+    df_out = condense_record(df_in, DEATH_RECORD_COLUMNS)
+    # Drop entries with no death date
+    df_out = df_out[pd.notna(df_out['death_date'])]
+    return df_out
+
+
+def get_records_for_pids_in_csv(lookfor, filename, col_name='PID'):
+    """Takes a CSV with a PID column and creates a Pandas DataFrame with all the record data for those PIDs.
+
+    lookfor (str): the regex pattern used to identify record types from their descriptions, e.g. '[Cc]ensus' for census
+    filename (str): the file name of the CSV to get the PIDs from
+    col_name (str): the name of the column that contains the PIDs
+    """
+    df_in = pd.read_csv(filename)
+    fss = FamilySearchSourcer()
+    df_out = pd.concat((fss.get_records_for_pid(pid, lookfor) for pid in df_in[col_name])).reset_index(drop=True)
     return df_out
 
 
 def get_census_for_pids_in_csv(filename, col_name='PID', saveas=None, condense=True, save_uncondensed=True):
-    """Takes a CSV with a PID column and creates a Pandas DataFrame with all the census data for those PIDs.
+    """Runs get_records_for_pids_in_csv, looking for census records. With options to condense results and save.
 
-    filename (str): the file name of the CSV to get the PIDs from
-    col_name (str): the name of the column that contains the PIDs
     saveas (str, optional): a file name to save the outputted DataFrame in CSV format
     condense (bool): whether or not to run condense_census on the data before outputting
     save_uncondensed (bool): if saveas isprovided and condense is True, determines whether to also save uncondensed data
     """
-    df_in = pd.read_csv(filename)
-    fss = FamilySearchSourcer()
-    df_out = pd.concat((fss.get_census_for_pid(pid) for pid in df_in[col_name])).reset_index(drop=True)
+    df_out = get_records_for_pids_in_csv(CENSUS_PTTRN, filename, col_name)
     if condense:
         if (saveas is not None) and save_uncondensed:
-            df_out.to_csv(re.sub(r'\.{3,4}$', '', saveas) + 'uncondensed.csv', index=False)
+            df_out.to_csv(re.sub(r'\..{3,4}$', '_uncondensed.csv', saveas), index=False)
         df_out = condense_census(df_out)
+    if saveas is not None:
+        df_out.to_csv(saveas, index=False)
+    return df_out
+    # TODO: Change so checks if the save file already exists and appends to it instead of creating it if it does already exist
+
+
+def get_deaths_for_pids_in_csv(filename, col_name='PID', saveas=None, condense=True, save_uncondensed=True):
+    """Runs get_records_for_pids_in_csv, looking for death records. With options to condense results and save."""
+    df_out = get_records_for_pids_in_csv(DEATH_PTTRN, filename, col_name)
+    if condense:
+        if (saveas is not None) and save_uncondensed:
+            df_out.to_csv(re.sub(r'\..{3,4}$', '_uncondensed.csv', saveas), index=False)
+        df_out = condense_record(df_out)
     if saveas is not None:
         df_out.to_csv(saveas, index=False)
     return df_out
